3b64a3b573a5e93822f6288f7aad93a6b8e1a209
AmanSong <C20325896@mytudublin.ie>
2022-01-26 22:05:13 +0000

    killing the cat

--- java/src/ie/tudublin/Cat.java
+++ java/src/ie/tudublin/Cat.java
@@ -6,4 +6,39 @@ public class Cat extends Animal
     {
         super(name);
     }
+
+    private int lives;
+    public int numlives()
+    {
+        return lives;
+    }
+
+    public void kill(int newlives)
+    {
+        lives = newlives;
+
+        while(lives > 0)
+        {
+            if(lives > 0)
+            {
+                lives -= 1;
+                System.out.println("Ouch!");
+            }
+        }
+
+        if(lives == 0)
+        {
+            System.out.println("Dead!");
+        }
+    }
+}
+
+class newcat
+{
+    public static void main(String[] args)
+    {
+        Cat ginger = new Cat("Ginger");
+
+        ginger.kill(9);
+    }
 }
