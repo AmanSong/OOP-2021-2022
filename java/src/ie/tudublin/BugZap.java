37598cad3c984faab679d494b31155ffd8435a09
AmanSong <C20325896@mytudublin.ie>
2022-02-02 18:40:28 +0000

    change

---
+++ java/src/ie/tudublin/BugZap.java
@@ -0,0 +1,21 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class BugZap extends PApplet
+{
+    public void settings()
+    {
+        size(400, 400);
+    }
+
+    public void setup()
+    {
+
+    }
+
+    public void draw()
+    {
+
+    }
+}
