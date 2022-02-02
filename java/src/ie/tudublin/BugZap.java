fa9414f995f1bc28aba934de934c9c845fd6816b
AmanSong <C20325896@mytudublin.ie>
2022-02-02 18:54:37 +0000

    save

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -4,6 +4,11 @@ import processing.core.PApplet;
 
 public class BugZap extends PApplet
 {
+    float playerX = 1;
+    float playerY = 2;
+    float playerWidth = 3;
+
+
     public void settings()
     {
         size(400, 400);
@@ -16,6 +21,16 @@ public class BugZap extends PApplet
 
     public void draw()
     {
+        drawPlayer(5, 5, 5);
+    }
 
+    public void drawPlayer(float x, float y, float width)
+    {
+        strokeWeight(1);
+        stroke(1, 10,10);
+
+        playerX = x;
+        playerY = y;
+        playerWidth = width;
     }
 }
