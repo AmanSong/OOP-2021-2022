3e9162544bce2de741209132fb3d157c00b9c719
AmanSong <C20325896@mytudublin.ie>
2022-02-09 17:27:12 +0000

    lab 3 code

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -4,19 +4,42 @@ import processing.core.PApplet;
 
 public class Loops extends PApplet
 {
+	float cx;
+	float cy;
 
 	public void settings()
 	{
 		size(500, 500);
+		cx = width / 2;
+		cy = height /2;
 	}
 
-	public void setup() {
+	public void setup() 
+	{
 		colorMode(HSB);
 		
 	}
 
-	
 	public void draw()
 	{	
+		background(0);
+		noStroke();
+
+		if (mouseX < cx && mouseY < cy)
+		{
+			rect(0, 0, cx, cy);
+		}
+		else if (mouseX > cx && mouseY < cy)
+		{
+			rect(cx, 0, cx, cy);
+		}
+		else if (mouseX < cx && mouseY > cy)
+		{
+			rect(0, cy, cx, cy);
+		}
+		else
+		{
+			rect(cx, cy, cx, cy);
+		}
 	}
 }
