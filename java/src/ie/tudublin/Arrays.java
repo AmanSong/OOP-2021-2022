7778d27ae80c0f68cee4bbd5fa601e654087a93d
AmanSong <C20325896@mytudublin.ie>
2022-02-28 11:22:05 +0000

    pie chart commit

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -13,10 +13,24 @@ public class Arrays extends PApplet{
     //new array but no elements allocated
     float[] array2;
 
+    float min = rainfall[0];
+    float max = rainfall[0];
+    int maxIndex;
+    int mode = 0;
+
     public void settings()
     {
         size(500, 500);
     }
+
+    public void keyPressed()
+	{
+		if (key >= '0' && key <='9')
+		{
+			mode = key - '0';
+		}
+		println(mode);
+	}
 
     public void setup()
     {
@@ -38,8 +52,6 @@ public class Arrays extends PApplet{
             println(rainfall[i] + "\t" + months[i]);
         }
 
-        float min = rainfall[0];
-        float max = rainfall[0];
         float totalRain = 0;
         float averageRain = 0;
 
@@ -52,6 +64,7 @@ public class Arrays extends PApplet{
             if(max < rainfall[i])
             {
                 max = rainfall[i];
+                maxIndex = i;
             }
 
             totalRain = totalRain + rainfall[i];
@@ -65,20 +78,94 @@ public class Arrays extends PApplet{
 
     public void draw()
     {   
-        background(0);
-        colorMode(HSB);
-        float w = width / (float)rainfall.length;
-
-        for(int i = 0; i < rainfall.length; i++)
+        switch(mode)
         {
-            float x= i*w;
-            noStroke();
-            int c = (int)map(i, 0, rainfall.length, 0, 255);
-            fill(c, 255, 255);
-            rect(x, height, w, -rainfall[i]);
-            textAlign(CENTER,CENTER);
-            fill(0);
-            text(months[i], x + (w/2), height-10);
+            case 0:
+            {
+                background(0);
+                colorMode(HSB);
+                float w = width / (float)rainfall.length;
+                noStroke();
+                for (int i = 0 ; i < rainfall.length ; i++)
+                {
+                    float x = map(i, 0, rainfall.length, 0, width);
+                    int c = (int)map(i, 0, rainfall.length, 0, 255);
+
+                    fill(c, 255, 255);
+
+                    float h = map(rainfall[i], 0, rainfall[maxIndex], 0, -height);
+
+                    rect(x, height, w, h);
+                    fill(255);
+                    textAlign(CENTER, CENTER);
+                    text(months[i], x + (w / 2), height - 10);
+                }
+                break;
+            }
+            case 1:
+            {
+            //BAR CHART
+                background(0);
+                float border = width * 0.1f;
+                // draw the aixis
+                stroke(255);
+                line(border, border, border, height - border);
+                line(border, height - border, width - border, height - border);
+                for(int i = 0; i <= 120; i++)
+                {
+                    float y = map(i, 0, 120, height-border, border);
+                    line(border-5, y, border, y);
+                    fill(255);
+                    textAlign(CENTER,CENTER);
+                    text(i, (border/2), y);
+                }
+                float w = (width-(border*2.0f)) / (float)rainfall.length;
+                for(int i = 0; i < rainfall.length; i++)
+                {
+                    float x = map(i, 0, rainfall.length, border, width-border);
+                    float c = map(i, 0, rainfall.length, 0, 255);
+                    stroke(255);
+                    fill(c, 255, 255);
+                    float h = map(rainfall[i], 0, 120, 0, -height + (border*2));
+                    rect(x, height-border, w, h);
+                    fill(255);
+                    text(months[i], x+ (w/2), height-(border/2));
+                    textAlign(CENTER,CENTER);
+                }
+                break;
+            }
+
+            case 2:
+            //PIE CHART
+                background(0);
+                float r = width * 0.3f;
+                float cx = width / 2;
+                float cy = height / 2;
+                noFill();
+                stroke(255);
+                circle(cx, cy, r * 2.0f);
+                float total = 0;
+                for(int i = 0; i < rainfall.length; i++)
+                {
+                    total = total + rainfall[i];
+                }
+                float start = 0;
+                for(int i = 0; i <rainfall.length; i++)
+                {
+                    float val = map(rainfall[i], 0, total, 0, TWO_PI);
+                    float c = map(i, 0, rainfall.length, 0, 255);
+                    stroke(255);
+                    fill(c, 255, 255);
+                    arc(cx, cy, r * 2,r * 2, start, start + val, PIE);
+
+                    float theta = start + (val * 0.5f);
+                    float x = cx + cos(theta) * (r * 1.2f);
+                    float y = cy + sin(theta) * (r * 1.2f);
+                    fill(255);
+                    text(months[i], x, y);
+
+                    start = start + val;
+                }
         }
     }
 }
